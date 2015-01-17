5d12655d7815145c00d6b4d3be6a5b09d308f496
Danielle Manalastas <danielle.manalastas@gmail.com>
2015-01-17 12:48:42 -0800

    Added Sweep and SweepLauncher

---
+++ spooder-mans/Assets/Scripts/Sweep.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sweep : MonoBehaviour 
+{
+	public GameObject SweepDetector;
+	private bool InputPressed;
+	private bool InputReleased = true;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (Input.GetButton ("Sweep1")) 
+			{
+				InputPressed = true;
+			} 
+		else 
+			{
+				InputPressed = false;
+				InputReleased = true;
+			}
+		if (InputPressed && InputReleased) 
+			{
+				SweepDetector.SetActive(true);
+			}
+	}
+}
